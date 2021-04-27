2dde39c4dbc5b6e30a2f4712fc9aeeebe64bec19
walidbenali <walidbenali@hexatribe.com>
2021-04-27 10:20:29 +0100

    Serveur_controller_and_views

---
+++ GestionRestau/Controllers/ServeursController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestionRestau.Models;
+using GestionRestau.Models.Context;
+
+namespace GestionRestau.Controllers
+{
+    public class ServeursController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServeursController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Serveurs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Serveurs.ToListAsync());
+        }
+
+        // GET: Serveurs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serveur = await _context.Serveurs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (serveur == null)
+            {
+                return NotFound();
+            }
+
+            return View(serveur);
+        }
+
+        // GET: Serveurs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Serveurs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nom,Telephone,Email,Adresse")] Serveur serveur)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(serveur);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serveur);
+        }
+
+        // GET: Serveurs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serveur = await _context.Serveurs.FindAsync(id);
+            if (serveur == null)
+            {
+                return NotFound();
+            }
+            return View(serveur);
+        }
+
+        // POST: Serveurs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nom,Telephone,Email,Adresse")] Serveur serveur)
+        {
+            if (id != serveur.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(serveur);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServeurExists(serveur.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(serveur);
+        }
+
+        // GET: Serveurs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var serveur = await _context.Serveurs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (serveur == null)
+            {
+                return NotFound();
+            }
+
+            return View(serveur);
+        }
+
+        // POST: Serveurs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var serveur = await _context.Serveurs.FindAsync(id);
+            _context.Serveurs.Remove(serveur);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServeurExists(int id)
+        {
+            return _context.Serveurs.Any(e => e.Id == id);
+        }
+    }
+}
