623e34332d9476e6c7df49d1e831c61861ae27ae
walidbenali <walidbenali@hexatribe.com>
2021-04-27 09:03:20 +0100

    My_Second_Comment

--- GestionRestau/Startup.cs
+++ GestionRestau/Startup.cs
@@ -20,7 +20,7 @@ namespace GestionRestau
         {
             Configuration = configuration;
         }
-
+        //Mon deuxième commentaire
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
